a20073d45b2986f18fb561dfb272422f593d5981
Tboycodex <152282498+Tboycodex@users.noreply.github.com>
2023-11-29 17:26:57 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,13 +2,13 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-4.4.4" # the link of manifest
-export BRANCH="twrp-4.4.4" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0" # the link of manifest
+export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="msm8916" # codename used in device tree
-export DT_LINK="https://github.com/Tboycodex/Device-tree" # device tree link
-export DT_BRANCH="twrp" # device tree branch
+export DEVICE="KB7" # codename used in device tree
+export DT_LINK="https://github.com/Tboycodex/android_device_tecno_TECNO-KB7" # device tree link
+export DT_BRANCH="" # device tree branch
 export VENDOR="qualcomm" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
